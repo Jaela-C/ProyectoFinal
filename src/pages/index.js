9f0d240507451f5920584a874c5512c7284e3bf0
ChristianChalacan <66123229+ChristianChalacan@users.noreply.github.com>
2021-06-18 20:25:32 -0500

    revision de pagina principal

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,19 @@
+import MediaCard from '@/components/Carousel';
+import CardInformation from '@/components/CardInformation'
+import InformationAdmin from '@/components/InformationAdmin'
+import InformationUser from '@/components/InformationUser'
+import AppMovil from '@/components/AppMovil'
+import {ThemeProvider} from "@material-ui/core";
+
 export default function Home() {
   return (
     <div>
-      
+      <MediaCard></MediaCard>
+      <CardInformation></CardInformation>
+      <InformationAdmin></InformationAdmin>
+      <MediaCard></MediaCard>
+      <InformationUser></InformationUser>
+      <AppMovil></AppMovil>
     </div>
   )
 }
