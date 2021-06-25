4c7e3f34112e641af775e1b241b625da46fd3bda
ChristianChalacan <66123229+ChristianChalacan@users.noreply.github.com>
2021-06-25 11:34:58 -0500

    pagina de publicaciones para usuarios registrados

---
+++ src/pages/publications/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CardPublication from "@/components/CardPublication";
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        margin: '1% 7%',
+        display: "flex",
+        color: "black",
+
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+}));
+
+const publications = () =>{
+    const classes = useStyles();
+
+    return(
+        <div className={classes.root}>
+            <Grid container spacing={2}>
+                <Grid item xs={6}>
+                    <CardPublication/>
+                </Grid>
+                <Grid item xs={6}>
+                    <CardPublication/>
+                </Grid>
+                <Grid item xs={6}>
+                    <CardPublication/>
+                </Grid>
+                <Grid item xs={6}>
+                    <CardPublication/>
+                </Grid>
+            </Grid>
+        </div>
+
+    );
+}
+export default publications;
