8e8e5999304dc4d7f07d85598fbec04a60f57861
ChristianChalacan <66123229+ChristianChalacan@users.noreply.github.com>
2021-07-04 10:15:21 -0500

    edicion de publicaciones

---
+++ src/pages/publications/edit.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import EditionPublication from "@/components/EditionPublication";
+
+
+const New = () =>{
+    return(
+        <EditionPublication/>
+    );
+}
+export default New;
