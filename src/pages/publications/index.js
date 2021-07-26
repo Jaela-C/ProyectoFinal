efefa3dc42aebab2e0ce18997476b78b58814b8c
ChristianChalacan <66123229+ChristianChalacan@users.noreply.github.com>
2021-07-26 15:01:24 -0500

    arreglo del publicaciones y modal de cada publicacion

--- src/pages/publications/index.js
+++ src/pages/publications/index.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import CardPublication from "@/components/CardPublication";
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import { db } from '../../../firebase/initFirebase';
+import { useAuth } from '../../hocs/useAuth';
+import { Hidden } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -17,16 +19,88 @@ const useStyles = makeStyles((theme) => ({
         textAlign: 'center',
         color: theme.palette.text.secondary,
     },
+    container:{
+        overflow:'hidden',
+    },
 }));
 
 const publications = () =>{
-    const classes = useStyles();    
+    const classes = useStyles(); 
+    
+    const { user, onAuth } = useAuth();
+    const listPublications = [];
+    const [dataPublications, setDataPublications] = useState([]);
+    const handleDelete = async(id) => {
+        try {
+            await doDelete(id);
+        } catch (error) {
+            if (error.response) {
+                console.error(error.response);
+            } else if (error.request) {
+                console.error(error.request);
+            } else {
+                console.error("Error", error.message);
+            }
+            console.error(error.config);
+        }
+    }
+    useEffect(()=>{
+        onAuth()
+        if(user){
+            const getPublications = async () => {
+                await db.collection('foundations').doc(`${user.uid}`).collection('publications').onSnapshot(publication => {
+                    publication.forEach(doc => {
+                        const dataPublication = {
+                            date_ex: doc.data().date_ex,
+                            description: doc.data().description,
+                            image: doc.data().image,
+                            last_name: doc.data().last_name,
+                            name: doc.data().name,
+                            phone: doc.data().phone,
+                            title: doc.data().title,
+                        };
+                        listPublications.push({ id: doc.id, ...dataPublication});
+                  });
+                  setDataPublications(listPublications);
+                })
+            };
+            getPublications();
+        }
+    },[user]);
+    /*const getPublications = async () => {
+        await db.collection('foundations').doc(`${user.uid}`).collection('publications').onSnapshot(publication => {
+            publication.forEach(doc => {
+                const dataPublication = {
+                    date_ex: doc.data().date_ex,
+                    description: doc.data().description,
+                    image: doc.data().image,
+                    last_name: doc.data().last_name,
+                    name: doc.data().name,
+                    phone: doc.data().phone,
+                    title: doc.data().title,
+                };
+                listPublications.push({ id: doc.id, ...dataPublication});
+          });
+          setDataPublications(listPublications);
+        })
+    };*/
+
+    console.log('lista publicaciones', dataPublications)
+
     return(
         <div className={classes.root}>
-            <Grid container spacing={2}>
-                <Grid item xs={6}>
-                    <CardPublication/>
-                </Grid>
+            <Grid container spacing={2} className={classes.container}>
+            {console.log('mensaje 1', dataPublications)}
+            {
+                dataPublications.map((data) => {
+                    console.log('data', data)
+                    return(
+                        <Grid item xs={6}>
+                            <CardPublication props = {data}/>
+                        </Grid>
+                    );
+                })
+            }
             </Grid>
         </div>
     );
