085159767895d960979a149de30d2f738d9ea390
Jaela-C <jaelacarlosama3c@hotmail.com>
2021-07-29 10:05:27 -0500

    Cambio credenciales

--- firebase/initFirebase.js
+++ firebase/initFirebase.js
@@ -6,26 +6,20 @@ import 'firebase/analytics';
 import 'firebase/performance';
 import 'firebase/database';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBmwyIAILEIciWGKr7Y4-ir_R_QLC45ymE",
-    authDomain: "fundaciones-quito.firebaseapp.com",
-    databaseURL: "https://fundaciones-quito-default-rtdb.firebaseio.com",
-    projectId: "fundaciones-quito",
-    storageBucket: "fundaciones-quito.appspot.com",
-    messagingSenderId: "434057459017",
-    appId: "1:434057459017:web:17662afb190fa49dd49698",
-    measurementId: "G-1NQ1Y66S00"
-};
+const firebaseConfig = ({
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+});
+
 
     if(!firebase.apps.length){
         firebase.initializeApp(firebaseConfig);
         console.log('Conectado con firebase');
-        if(typeof window !== 'undefined'){
-            if('measurementId' in firebaseConfig){
-                firebase.analytics();
-                firebase.performance()
-            }
-        }
     }
     const app = firebase.app();
     const auth = firebase.auth();
