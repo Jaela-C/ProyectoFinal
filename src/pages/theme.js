1879325cc30e0d9a2f326451ba9123d8f72cd55a
Jaela-C <66124353+Jaela-C@users.noreply.github.com>
2021-09-15 05:47:53 -0500

    Cambios temp

---
+++ src/pages/theme.js
@@ -0,0 +1,31 @@
+import {createMuiTheme} from '@material-ui/core/styles';
+import {red} from '@material-ui/core/colors';
+
+// Create a theme instance.
+const theme = createMuiTheme({
+    palette: {
+        blueRYB: {
+            main: '#3E54E7',
+        },
+        azure: {
+            main: '#5081E5',
+        },
+        babyBlue: {
+            main: '#9CBBF2',
+        },
+        brinkPink:{
+            main: '#F06177',
+        },
+        imperialRed:{
+            main: '#EC323D'
+        },
+        error: {
+            main: red.A400,
+        },
+        background: {
+            default: '#E8F5FF',
+        },
+    },
+});
+
+export default theme;
